945a28f298b39f4ff83c05bd734d60c56c3ca06a
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-07-14 21:23:30 +0530

    Update room_automaton_new_main.ino

--- room automation/room_automaton_new_main/room_automaton_new_main.ino
+++ room automation/room_automaton_new_main/room_automaton_new_main.ino
@@ -1,69 +1,18 @@
+#include "constants.h"
 #include <IRremote.h>
-#include <ESP8266WiFi.h>
 #include <SinricPro.h>
 #include <SinricProSwitch.h>
-
-// Change Your Variables Here
-char ssid[] = "192.168.1.42";
-char pass[] = "Luffy@1414";
-char appkey[] = "065cd449-3fed-4437-b355-9705596b1c40";
-char appsec[] = "e43cc52f-59e0-4679-889b-d7609ddcb667-e7893776-fad8-445b-9459-11f69755aad3";
-
-#ifdef ENABLE_DEBUG
-       #define DEBUG_ESP_PORT Serial
-       #define NODEBUG_WEBSOCKETS
-       #define NDEBUG
-#endif 
-
 #include <Arduino.h>
-#ifdef ESP8266 
-       #include <ESP8266WiFi.h>
-#endif 
-#ifdef ESP32   
-       #include <WiFi.h>
-#endif
+#include <WiFiClientSecure.h>
 
-// Pin Layout for esp8266
 #ifdef ESP8266
-#define IR_INPUT D0
-#define ROOM_LIGHT D1
-#define SPEAKER D2
-#define FAN D3
-#define LAMP D4
-#define RED_LED D5
+  #include <BlynkSimpleEsp8266.h>
 #endif
-
-#include <BlynkSimpleEsp8266.h>
-
-// Place Blynk Variables Here
-#define BLYNK_TEMPLATE_ID "TMPLosDiardQ"
-#define BLYNK_TEMPLATE_NAME "Room Automation No Sensor Input"
-#define BLYNK_AUTH_TOKEN "ffSABDTkcCDoDV6j3rBSA2-xFMqaBSLj"
 
 //Do Not Touch
 char auth[] = BLYNK_AUTH_TOKEN;
-
-// Defining values to the signals received form the remote button.
-#define c1 3125149440
-#define c2 3108437760
-#define c3 3091726080
-#define c4 3141861120
-#define c5 3208707840
-#define c6 3158572800
-#define c7 4161273600
-#define c8 3927310080
-#define c9 4127850240
-#define c0 3860463360
-#define cst 3910598400
-#define chash 4061003520
-#define cup 3877175040
-#define cdown 2907897600
-#define cleft 4144561920
-#define cright 2774204160
-#define cok 3810328320
-
-// Define the input for the IR-receiver module
 IRrecv IR(IR_INPUT);
+bool Trigger = false;
 
 //Get the button value for blynk
 BLYNK_WRITE(V1) {
@@ -75,32 +24,124 @@ BLYNK_WRITE(V3) {
 BLYNK_WRITE(V4) {
   digitalWrite(LAMP, param.asInt());}
 
-/*****************
- * Configuration *
- *****************/
+// Structure of Relay Info
 struct RelayInfo {
   String deviceId;
   String name;
   int pin;
 };
 
-// Declaring The Number of relays 
+// Declaring The Number of relays for siniric pro
 std::vector<RelayInfo> relays = {
-    {"6443b1df918a3c911c862a54", "Relay 1", ROOM_LIGHT},
+    {"64b123092ac6a1822a87bb5f", "Relay 1", ROOM_LIGHT},
     {"6444043e312d40edc3f889be", "Relay 2", SPEAKER},
     {"6444042b918a3c911c8675a5", "Relay 3", FAN}
 };
 
-#define WIFI_SSID  ssid
-#define WIFI_PASS  pass
-#define APP_KEY    appkey
-#define APP_SECRET appsec
-#define BAUD_RATE  9600              // Change baudrate to your need
+// Function Declaration
+void setupWiFi();
+void setupRelayPins();
+void setupSinricPro();
+void on(int);
+void off(int);
+bool onPowerState(const String &deviceId, bool &state);
+
+//=====================================================================
+// Void Setup
+//=====================================================================
+void setup() {
+  IR.enableIRIn();
+  pinMode(IR_INPUT, INPUT);     // IR remote input
+  pinMode(ROOM_LIGHT, OUTPUT);  // Room Light
+  pinMode(SPEAKER, OUTPUT);     // Speaker
+  pinMode(FAN, OUTPUT);         // FAn
+  pinMode(LAMP, OUTPUT);     // Red LEd
+  digitalWrite(ROOM_LIGHT, LOW);
+  digitalWrite(FAN, LOW);   
+  digitalWrite(LAMP, HIGH);
+
+  setupRelayPins();       
+  setupSinricPro();
+
+  Serial.begin(BAUD_RATE);
+  setupWiFi();
+  if (WiFi.status() == WL_CONNECTED) {
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  digitalWrite(LAMP, LOW);
+  }
+  else{
+  digitalWrite(LAMP, LOW);
+  Trigger = true;
+  //Serial.println("No wofi Found");}
+ }
+} 
+
+//=====================================================================
+// Void Loop
+//=====================================================================
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+  if (Trigger == true){
+      Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+      //Serial.println("Trigger Section");
+      Trigger = false;
+      return;
+    }  
+  Blynk.run();
+  SinricPro.handle();
+  if (IR.decode()) {
+    handleIR();
+   }
+  }
+  else{
+    if (IR.decode()) {
+    handleIR();
+    }
+  }
+}
+
+//---------------------------------------------------------------------
+// Alll The aBOVE CALLED functiona are here
+//---------------------------------------------------------------------
+
+void setupWiFi() {
+  Serial.println("\r\n[Wifi]: Connecting...");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  unsigned long startTime = millis();
+  const unsigned long timeout = WIFI_TIMEOUT;  // Timeout set to 10 seconds (10,000 milliseconds)
+
+  while (WiFi.status() != WL_CONNECTED) {
+    if (IR.decode()) {
+    handleIR();
+    }
+    if (millis() - startTime >= timeout) {
+      Serial.println("\r\n[Wifi]: Connection timed out");
+      return;  // Exit the function if timeout is reached
+    }
+    Serial.print(".");
+    delay(500);
+  }
+
+  //wifiConnected = true;  // Set flag to true if Wi-Fi connection is established
+  Serial.printf("\r\n[Wifi]: Connected\r\n[Wifi]: SSID --> %s\r\n[Wifi]: IP-Address --> %s\r\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
+}
+
+void setupSinricPro() {
+  for (auto &relay : relays) {                             // for each relay configuration
+    SinricProSwitch &mySwitch = SinricPro[relay.deviceId];   // create a new device with deviceId from relay configuration
+    mySwitch.onPowerState(onPowerState);                     // attach onPowerState callback to the new device
+  }
+  SinricPro.onConnected([]() { Serial.printf("Connected to SinricPro\r\n"); });
+  SinricPro.onDisconnected([]() { Serial.printf("Disconnected from SinricPro\r\n"); });
+  SinricPro.begin(APP_KEY, APP_SECRET);
+}
 
 bool onPowerState(const String &deviceId, bool &state) {
   for (auto &relay : relays) {                                                            // for each relay configuration
     if (deviceId == relay.deviceId) {                                                       // check if deviceId matches
-      Serial.printf("Device %s turned %s\r\n", relay.name.c_str(), state ? "on" : "off");     // print relay name and state to serial
+      //Serial.printf("Device %s turned %s\r\n", relay.name.c_str(), state ? "on" : "off");     // print relay name and state to serial
       digitalWrite(relay.pin, state);                                                         // set state to digital pin / gpio
       return true;                                                                            // return with success true
     }
@@ -114,118 +155,52 @@ void setupRelayPins() {
   }
 }
 
-void setupWiFi() {
-  Serial.printf("\r\n[Wifi]: Connecting");
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
+void handleIR(){
+  //Serial.println(IR.decodedIRData.decodedRawData, DEC);  //Shows the read value on the monitor screen(if exisits).
+    switch (IR.decodedIRData.decodedRawData) {
+      case C1:
+        on(ROOM_LIGHT);
+        break;
 
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.printf(".");
-    delay(250);
-  }
-  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
-}
+      case C4:
+        off(ROOM_LIGHT);
+        break;
 
-void setupSinricPro() {
-  for (auto &relay : relays) {                             // for each relay configuration
-    SinricProSwitch &mySwitch = SinricPro[relay.deviceId];   // create a new device with deviceId from relay configuration
-    mySwitch.onPowerState(onPowerState);                     // attach onPowerState callback to the new device
-  }
+      case C2:
+        on(SPEAKER);
+        break;
 
-  SinricPro.onConnected([]() { Serial.printf("Connected to SinricPro\r\n"); });
-  SinricPro.onDisconnected([]() { Serial.printf("Disconnected from SinricPro\r\n"); });
+      case C5: 
+        off(SPEAKER);
+        break;
 
-  SinricPro.begin(APP_KEY, APP_SECRET);
-}
+      case C3:
+        on(FAN);
+        break;
 
-void setup() {
-  IR.enableIRIn();
-  pinMode(IR_INPUT, INPUT);     // IR remote input
-  pinMode(ROOM_LIGHT, OUTPUT);  // Room Relay
-  pinMode(SPEAKER, OUTPUT);     // Speaker
-  pinMode(FAN, OUTPUT);         // FAn
-  pinMode(RED_LED, OUTPUT);     // Red LEd
-  pinMode(LAMP, OUTPUT);     // Red LEd
+      case C6:
+        off(FAN);
+        break;
 
-  digitalWrite(ROOM_LIGHT, LOW);  //default on
-  digitalWrite(FAN, LOW);         //default fan  on
-  digitalWrite(LAMP, HIGH);         //default fan  on 
+      case CST:
+        on(LAMP);
+        break;
 
-  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
-  Serial.begin(BAUD_RATE);
-  setupRelayPins();
-  setupWiFi();
-  setupSinricPro();
-  digitalWrite(LAMP, LOW);         //Turn the lamp off only after wifi gets connected
-}
+      case CHASH:
+        off(LAMP);
+        break;  
+
+      case C0:  //Press 0 to turn off all appliances
+        off(ROOM_LIGHT);
+        off(SPEAKER);
+        off(FAN);
+        off(LAMP);
+    }
+    IR.resume();
+   }
 
 // on/off Modules
 void on(int p) {
   digitalWrite(p, HIGH);}
 void off(int p) {
   digitalWrite(p, LOW);}
-
-void loop() {
-  SinricPro.handle();
-  Blynk.run();
-  if (IR.decode()) {
-    //Serial.println(IR.decodedIRData.decodedRawData, DEC);  //Shows the read value on the monitor screen(if exisits).
-    switch (IR.decodedIRData.decodedRawData) {
-      case c1:           //press 1 to turn on 1st switch.
-                         // Serial.println(" Room Light ON");
-        on(ROOM_LIGHT);  // Turns on Relay
-        off(RED_LED);    // Turns off Red LED
-        break;
-
-      case c4:  //press 4 to turn off 1st switch.
-        //Serial.println(" Room Light OFF");
-        off(ROOM_LIGHT);  // Turns of Relay
-        on(RED_LED);      // Turns on Red LED
-        break;
-
-      case c2:  //press 2 to turn on 2nd switch.
-        //Serial.println(" BathRoom Light ON");
-        on(SPEAKER);
-        off(RED_LED);
-        break;
-
-      case c5:  //press 5 to turn off 2nd switch.
-        //Serial.println(" BathRoom Light OFF");
-        off(SPEAKER);
-        on(RED_LED);
-        break;
-
-      case c3:
-        on(FAN);
-        off(RED_LED);
-        break;
-
-      case c6:
-        off(FAN);
-        on(RED_LED);
-        break;
-
-      case cst:
-        on(LAMP);
-        off(RED_LED);
-        break;
-
-      case chash:
-        off(LAMP);
-        on(RED_LED);
-        break;  
-
-      case c0:  //Press 0 to turn off all appliances
-        //Serial.println(" All Lights OFF");
-        on(RED_LED);  // Turns on Red LED
-        off(ROOM_LIGHT);
-        off(SPEAKER);
-        off(FAN);
-        off(LAMP);
-        /*off(D5);
-        off(D6);
-        off(D7);
-        off(D8);*/
-    }
-    IR.resume();
-  }
-}
