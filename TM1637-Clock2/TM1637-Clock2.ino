469acd9614fc5d22aaeabc0e99d292f620eafcfd
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-08-27 09:29:22 +0530

    Update TM1637-Clock2.ino

--- TM1637-Clock2/TM1637-Clock2.ino
+++ TM1637-Clock2/TM1637-Clock2.ino
@@ -2,6 +2,7 @@
 #include <ESP8266WiFi.h>
 #include "time.h"
 
+#define BRIGHTNESS  3   //0-7
 const char* ssid = "Kens Device";
 const char* password = "asdfghjkl";
 const int CLK_PIN = D2;
@@ -20,14 +21,14 @@ TM1637Display display(CLK_PIN, DIO_PIN);
 void setup()
 {
   Serial.begin(9600);
-  display.setBrightness(7); // Set display brightness
+  display.setBrightness(BRIGHTNESS); // Set display brightness
   display.clear();
   //connect to WiFi
   Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
       display.showNumberDec(i, false);
-      delay(500);
+      delay(1000);
       i++;
   }
   //Serial.println("CONNECTED to WIFI");
@@ -74,7 +75,7 @@ void printLocalTime()
   ab = (hours);
   ab +=  (minutes);
   int num = ab.toInt();
-  display.showNumberDecEx(num,colonStat);
+  display.showNumberDecEx(num,colonStat,true,4,0);   //true to show preceeding 0's,4 for no of digits, 0 for starting index
 
   //Serial.printf("Current time: %02d:%02d\n", hours, minutes);
   //Serial.println(asctime(timeinfo));
