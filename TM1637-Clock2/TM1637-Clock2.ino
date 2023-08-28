8395ea115127f8f8e9c6b58af22cc04229855bac
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-08-28 20:52:54 +0530

    Update TM1637-Clock2.ino

--- TM1637-Clock2/TM1637-Clock2.ino
+++ TM1637-Clock2/TM1637-Clock2.ino
@@ -2,19 +2,21 @@
 #include <ESP8266WiFi.h>
 #include "time.h"
 
-#define BRIGHTNESS  3   //0-7
-const char* ssid = "Kens Device";
-const char* password = "asdfghjkl";
-const int CLK_PIN = D2;
-const int DIO_PIN = D3;
+#define ssid "Kens Device"
+#define password "asdfghjkl"
+#define BRIGHTNESS  2   //0-7
+#define SHOW_DATE true
+#define CLK_PIN D0
+#define DIO_PIN D1
+
+String ab="";
 int colonShow = 0b01000000;
 int colonHide = 0b00000000;
-int colonStat,i=0;
+int colonStat,i=0,j=0;
 bool colonVisible = true;
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 19800;   //Replace with your GMT offset (seconds) 19800
 const int   daylightOffset_sec = 0;  //Replace with your daylight offset (seconds)
-String ab="";
 TM1637Display display(CLK_PIN, DIO_PIN);
 
 
@@ -24,10 +26,10 @@ void setup()
   display.setBrightness(BRIGHTNESS); // Set display brightness
   display.clear();
   //connect to WiFi
-  Serial.printf("Connecting to %s ", ssid);
+  //Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-      display.showNumberDec(i, false);
+      display.showNumberDec(i, true);
       delay(1000);
       i++;
   }
@@ -53,9 +55,11 @@ void printLocalTime()
   time (&rawtime);
   timeinfo = localtime (&rawtime);
 
-  // Extract hour and minutes
+  // Extract hour and minutes and date and month
   int hours = timeinfo->tm_hour;
   int minutes = timeinfo->tm_min;
+  int date = timeinfo->tm_mday;
+  int month = timeinfo->tm_mon;
   //const char* ampm = (hours < 12) ? "AM" : "PM";
 
   // Convert to 12-hour format
@@ -65,6 +69,20 @@ void printLocalTime()
   if (hours == 0) {
     hours = 12;
   }
+  #if SHOW_DATE == true
+    if (j==14){
+      display.clear();
+      hours = date;
+      minutes = month;
+    }
+
+    else if (j==15){
+      display.clear();
+      hours = date;
+      minutes = month;
+      j=0;
+    }
+  #endif  
 
   // Make the colon Blink 
   colonVisible = !colonVisible;
@@ -92,5 +110,6 @@ void printLocalTime()
   //Serial.println(num);
   display.showNumberDecEx(num,colonStat,true,4,0);   //true to show preceeding 0's,4 for no of digits, 0 for starting index
   //Serial.println(asctime(timeinfo));
+  j++;
   delay(1000);
 }
