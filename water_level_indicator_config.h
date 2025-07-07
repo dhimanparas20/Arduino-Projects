a24f2c6d43a470af80faca4401c7dd08a41b0b4c
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2025-07-07 12:10:54 +0530

    Update water_level_indicator_config.h

--- water_level_indicator_config.h
+++ water_level_indicator_config.h
@@ -6,9 +6,9 @@ const char* ssid = "MST Protocol";
 const char* password = "Blackkaiser69";
 
 // MQTT server configuration
-#define MQTT_SERVER ""
-#define MQTT_USER ""
-#define MQTT_PASSWORD ""
+#define MQTT_SERVER "mqtt.mstservices.tech"
+#define MQTT_USER "mst"
+#define MQTT_PASSWORD "1212"
 #define MQTT_PORT 1883
 #define WS_DEVICE_CLIENTID "9POWuKe/nodemcuwatertank"
 
@@ -17,14 +17,17 @@ const char* publishTopic = "watertank/percentage";
 const char* subscribeTopicMinDist = "watertank/mindist";
 const char* subscribeTopicMaxDist = "watertank/maxdist";
 const char* subscribeTopicDisplay = "watertank/display";
+const char* subscribeTopicBuzzer = "watertank/buzzer";
 
 // Default threshold distance (in cm)
 const int defaultThresholdDistance = 10;
 
 // Variable to track the delay duration
-int currentDelay = 2000; // Start with 2000 ms delay
+int currentDelay = 5000; // Start with 5000 ms delay
 
 // Display control flag (default state)
 const bool defaultDisplayEnabled = true;
+// Buzzer control flag (default state)
+const bool defaultBuzzerSound = true;
 
 #endif // CONFIG_H
